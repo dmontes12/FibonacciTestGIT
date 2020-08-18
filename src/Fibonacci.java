8e090b3af4cd23a51c66834fe699cbc5d1617576
Dmontes <dgmontesg.12@gmail.com>
2020-08-18 12:55:16 -0600

    Fibonacci recursivo
    se agrega fibonacci recursivo y
    se puede elejir entre usar iterativo o recursivo

--- src/Fibonacci.java
+++ src/Fibonacci.java
@@ -1,3 +1,4 @@
+
 /**
 	Diego Montes
 */
@@ -6,31 +7,64 @@ import java.util.Scanner;
 
 public class Fibonacci {
 
+	public static int Fibonacci(int n) {
+
+		if (n == 1) {
+			return 0;
+		}
+		if (n == 2) {
+			return 1;
+		}
+
+		return Fibonacci(n - 1) + Fibonacci(n - 2);
+	}
+
+	public static int FibonacciIterativo(int n) {
+		int a = 0;
+		int b = 1;
+		int c = 0;
+
+		if (n == 1) {
+			return 0;
+		} else if (n == 2) {
+			return 1;
+		} else {
+			for (int i = 2; i < n; i++) {
+				c = a + b;
+				a = b;
+				b = c;
+			}
+			return b;
+		}
+
+	}
+
 	public static void main(String[] args) {
 		Scanner leer = new Scanner(System.in);
 		int indice = 0;
 
 		System.out.println("Ingresa el indice del numero que quieres obtener de la serie Fibonacci: ");
 		indice = leer.nextInt();
+		if (indice > 0) {
 
-		int a = 0;
-		int b = 1;
-		int c = 0;
-		if (indice < 0) {
-			System.out.println("Ingresa un numero mayor a 0 e intenta otra vez");
-		} else {
-			if (indice == 1) {
-				System.out.println(a);
-			} else if (indice == 2) {
-				System.out.println(b);
-			} else {
-				for (int i = 2; i < indice; i++) {
-					c = a + b;
-					a = b;
-					b = c;
-				}
-				System.out.println("El numero con indice " + indice + " es: " + b);
+			int opcion = 0;
+			System.out.println("Elija la opcion \n1.-Iterativa\n2.- Recursiva");
+			opcion = leer.nextInt();
+
+			switch (opcion) {
+
+			case 1:
+				System.out.println("El numero: " + FibonacciIterativo(indice));
+				break;
+
+			case 2:
+				System.out.println("El numero: " + FibonacciIterativo(indice));
+				break;
+			default:
+				System.out.println("Elije una opcion valida");
 			}
+		} else {
+			System.out.println("Ingresa un numero mayor a 0");
 		}
 	}
 
